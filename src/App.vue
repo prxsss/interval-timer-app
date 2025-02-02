af91e30387a6a2a128d86f491010008b4c8634d1
prxsss <9phuriphat.k@gmail.com>
2025-02-02 17:59:16 +0700

    add Tailwind CSS, daisyUI and Prettier configuration; remove HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+    <div
+        class="flex min-h-screen w-full items-center justify-center bg-base-200"
+    >
+        <div
+            class="flex h-[550px] w-full max-w-4xl items-center justify-center rounded-xl bg-base-100 shadow"
+        >
+            <h1 class="text-3xl font-bold">Hello, World!</h1>
+        </div>
+    </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
