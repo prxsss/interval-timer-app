51befbfb91651787269353de62bc52713dd8fb29
prxsss <9phuriphat.k@gmail.com>
2025-02-03 17:00:17 +0700

    add primeicons dependency; enhance App.vue with interval management and sound toggle; create IntervalListing and TimerModal components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,81 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import soundFile from './assets/sounds/mixkit-mouse-click-close-1113.wav'
+
+import IntervalListing from './components/IntervalListing.vue'
+
+const rounds = ref(1)
+const intervals = ref(1)
+
+const incrementRounds = () => {
+    rounds.value++
+}
+
+const decrementRounds = () => {
+    if (rounds.value > 1) {
+        rounds.value--
+    }
+}
+
+const toggleSound = () => {
+    const soundEffect = new Audio(soundFile)
+    soundEffect.play()
+}
+</script>
 
 <template>
     <div
         class="flex min-h-screen w-full items-center justify-center bg-base-200"
     >
         <div
-            class="flex h-[550px] w-full max-w-4xl items-center justify-center rounded-xl bg-base-100 shadow"
+            class="w-full max-w-3xl space-y-6 rounded-xl bg-base-100 p-6 shadow"
         >
-            <h1 class="text-3xl font-bold">Hello, World!</h1>
+            <div class="text-center text-3xl font-semibold">00:00</div>
+            <div class="space-x-4 text-center">
+                <button class="btn btn-neutral text-lg">
+                    <i class="pi pi-play"></i>
+                </button>
+                <button class="btn btn-neutral text-lg">
+                    <i class="pi pi-refresh"></i>
+                </button>
+            </div>
+            <div class="flex items-center justify-between">
+                <div class="space-x-4">
+                    <span class="text-lg font-semibold">Rounds: </span>
+                    <button @click="decrementRounds" class="btn">
+                        <i class="pi pi-minus"></i>
+                    </button>
+                    <span>{{ rounds }}</span>
+                    <button @click="incrementRounds" class="btn">
+                        <i class="pi pi-plus"></i>
+                    </button>
+                </div>
+                <div class="form-control">
+                    <label class="label cursor-pointer space-x-2">
+                        <span class="label-text">Sound</span>
+                        <input
+                            @change="toggleSound"
+                            type="checkbox"
+                            class="toggle"
+                        />
+                    </label>
+                </div>
+            </div>
+            <div class="max-h-72 space-y-4 overflow-y-auto">
+                <IntervalListing
+                    v-for="(_, index) in new Array(intervals)"
+                    :id="index"
+                    :key="index"
+                />
+            </div>
+            <div>
+                <button
+                    @click="intervals++"
+                    class="btn btn-neutral btn-sm w-full"
+                >
+                    Add Interval
+                </button>
+            </div>
         </div>
     </div>
 </template>
---
+++ src/components/IntervalListing.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import TimerModal from './TimerModal.vue'
+
+const props = defineProps<{
+    id: number
+}>()
+
+const openModal = () => {
+    const modal = document.getElementById(
+        `my_modal_${props.id}`
+    ) as HTMLDialogElement
+    if (modal) {
+        modal.showModal()
+    }
+}
+</script>
+
+<template>
+    <div class="flex items-center space-x-4 rounded-xl bg-base-200">
+        <div class="cursor-move -space-x-2 py-4 pl-4 text-sm">
+            <i class="pi pi-ellipsis-v"></i>
+            <i class="pi pi-ellipsis-v"></i>
+        </div>
+        <div
+            @click="openModal"
+            class="flex flex-1 cursor-pointer items-center justify-between py-4 pr-4 font-medium"
+        >
+            <div>IntervalListing</div>
+            <div>0:00</div>
+        </div>
+    </div>
+    <TimerModal :modal-id="`my_modal_${props.id}`" />
+</template>
