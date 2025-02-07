5a2137dcb76573ad20e87c5491894d375ca1887c
prxsss <9phuriphat.k@gmail.com>
2025-02-07 13:35:00 +0700

    add theme-change functionality; update index.html for theme support; integrate Catppuccin themes in Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from 'daisyui';
+import catppuccin from '@catppuccin/daisyui';
 
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
@@ -7,4 +8,20 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+  daisyui: {
+    // The top value of this array will be used as the default theme
+    // You can use https://github.com/saadeghi/theme-change to switch between themes
+    themes: [
+      // You can simply select a catppuccin flavor with sane default colors
+      catppuccin('latte'),
+      // Or you can optionally specify accent colors
+      catppuccin('frappe', 'pink'),
+      // Or you can optionally customize more semantic colors
+      catppuccin('macchiato', { primary: 'maroon' }),
+      // Values not explicitly defined will use default values
+      catppuccin('mocha', { primary: 'sky', secondary: 'rosewater' }),
+      // Fallback to default theme
+      'light',
+    ],
+  },
 };
